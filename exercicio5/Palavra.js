e47d86155035c5c2dca14dc35213fdcd0ed37c41
isaque araujo nogueira <isaquean2@gmail.com>
2021-11-11 20:40:05 -0300

    adicionados comentarios de documentacao

--- exercicio5/Palavra.js
+++ exercicio5/Palavra.js
@@ -2,6 +2,11 @@ export default class Palavra {
     constructor() {
         this.vogais = "aáàâãeéèễẽiiíìîĩoóòôõuúùûũ";
     }
+    /**
+     *
+     * @param texto - texto do input
+     * @returns numero de vogais contidas no texto do input
+     */
     contadorDeVogais(texto) {
         let numeroDeVogais = 0;
         for (let letra of texto.toLowerCase()) { // conta a quantidade de vogais
--- exercicio5/Palavra.ts
+++ exercicio5/Palavra.ts
@@ -1,6 +1,11 @@
 export default class Palavra {
     public vogais: string = "aáàâãeéèễẽiiíìîĩoóòôõuúùûũ";
 
+    /**
+     * 
+     * @param texto - texto do input
+     * @returns numero de vogais contidas no texto do input
+     */
     public contadorDeVogais(texto: string): number {
         let numeroDeVogais: number = 0;
     
